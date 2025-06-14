f07a7887466620e38d2173e568110ae9e894cc76
shironeek0 <shironeek@gmail.com>
2025-06-14 23:08:31 +0800

    organize code

--- main.lua
+++ main.lua
@@ -1,26 +1,38 @@
+player = {
+    x = 400,
+    y = 200,
+    w = 200,
+    h = 200,
+}
+
+platform = {
+    x = 400,
+    y = 500,
+    w = 300,
+    h = 100,
+}
+
 function love.load()
-    player = {}
-    player.x = 400
-    player.y = 200
-    player.speed = 3
+    speed = 3
 end
 
 function love.update(dt)
     if love.keyboard.isDown('d') then
-        player.x = player.x + player.speed
+        player.x = player.x + speed
     end
     if love.keyboard.isDown('a') then
-        player.x = player.x - player.speed
+        player.x = player.x - speed
     end
     if love.keyboard.isDown('s') then
-        player.y = player.y + player.speed
+        player.y = player.y + speed
     end
     if love.keyboard.isDown('w') then
-        player.y = player.y - player.speed
+        player.y = player.y - speed
     end
     
 end
 
 function love.draw()
+    love.graphics.circle("fill", player.x, player.y, 100)
     love.graphics.circle("fill", player.x, player.y, 100)
 end
